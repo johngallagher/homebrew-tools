84dba847f8d0ced3f948544040f06e176aca252f
John Gallagher <john@synapticmishap.co.uk>
2018-05-02 11:55:49 +0100

    Update deploy_this.rb

--- deploy_this.rb
+++ deploy_this.rb
@@ -1,6 +1,6 @@
 class DeployThis < Formula
   desc "A command line tool to deploy to Cloud66"
-  url "https://github.com/johngallagher/deploy_this/raw/master/archive/deploy-this-1.0.0.tar.gz"
+  url "https://github.com/johngallagher/deploy_this/archive/1.0.0.tar.gz"
   sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
   version "1.0.0"
 
